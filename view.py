5d85cca99762be1782e405b1247c566ce6b722a7
HerrLixus <ivanglazkovoff@gmail.com>
2021-10-27 19:53:55 +0300

    created some ui, added test data

--- view.py
+++ view.py
@@ -1,1 +1,18 @@
-import PyQt5
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+from main_ui import Ui_MainWindow
+
+
+class Example(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
